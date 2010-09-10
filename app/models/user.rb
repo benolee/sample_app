56320572c2f5d5f94bad89708c2beb33cd592e85
Ben Holley <benolee@gmail.com>
2010-09-10 12:36:35 -0500

    [user-model] added password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,40 @@
+# == Schema Information
+# Schema version: 20100909210835
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, :presence => true,
+                   :length   => { :maximum => 50 }
+
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+  validates :password, :presence      => true,
+                       :confirmation  => true,
+                       :length        => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
 end
