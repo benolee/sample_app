7b5b8171752daf6f836eda4f04bbad22ff71ccbc
Ben Holley <benolee@gmail.com>
2010-09-13 10:27:28 -0500

    [user-model] added factories, gravatar

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                   "Lloyd"
-  user.email                  "Wright"
+  user.name                   "Lloyd Smith"
+  user.email                  "lsmith@example.com"
   user.password               "foobar"
   user.password_confirmation  "foobar"
 end
