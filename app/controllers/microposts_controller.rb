333301005ecbc9e949ebba9c8efd3d8286eda8d0
Ben Holley <benolee@gmail.com>
2010-09-16 12:58:13 -0500

    Added prototype of status feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,12 @@ class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+    else
+      render 'pages/home'
+    end
   end
 
   def destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -147,6 +147,8 @@ describe User do
 
     before(:each) do
       @user = User.create(@attr)
+      @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+      @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
     end
 
     it "should have a micropost attribute" do
@@ -161,6 +163,24 @@ describe User do
       @user.destroy
       [@mp1, @mp2].each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status feed" do
+
+      it "should have a feed" do
+        @user.should respond_to(:feed)
+      end
+
+      it "should include the user's microposts" do
+        @user.feed.include?(@mp1).should be_true
+        @user.feed.include?(@mp2).should be_true
+      end
+
+      it "should not include a different user's microposts" do
+        mp3 = Factory(:micropost,
+                      :user => Factory(:user, :email => Factory.next(:email)))
+        @user.feed.include?(mp3).should be_false
       end
     end
   end
